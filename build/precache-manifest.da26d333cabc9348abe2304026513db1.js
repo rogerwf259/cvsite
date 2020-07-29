4efbc5825abcf1de93ad582c2b73eb7bf230294f
rogerwf259 <rogeliomer_2@hotmail.com>
2020-07-29 11:36:09 -0500

    Add files via upload
    Initial commit

---
+++ build/precache-manifest.da26d333cabc9348abe2304026513db1.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cfa9bd1955bf25440fc9426bd59624e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ca8852805a6a356a741",
+    "url": "/static/css/main.39ddd4b9.chunk.css"
+  },
+  {
+    "revision": "edf5d453b9e2ae6a065c",
+    "url": "/static/js/2.d4619550.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.d4619550.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ca8852805a6a356a741",
+    "url": "/static/js/main.f2be142d.chunk.js"
+  },
+  {
+    "revision": "bcf5001c1f9c2f4dd60f",
+    "url": "/static/js/runtime-main.03e4a644.js"
+  },
+  {
+    "revision": "31939f83f992c8df1f9b6fd34355fa7f",
+    "url": "/static/media/atom.31939f83.svg"
+  },
+  {
+    "revision": "4b84c87f22013be438f64061b7feed13",
+    "url": "/static/media/clang.4b84c87f.svg"
+  },
+  {
+    "revision": "9a5142cdfe5d1242a59cd9f2d208536b",
+    "url": "/static/media/code.9a5142cd.svg"
+  },
+  {
+    "revision": "c8b0c4ed43e7d7a9eb82e25f24ab6098",
+    "url": "/static/media/contact.c8b0c4ed.svg"
+  },
+  {
+    "revision": "dd5dfed3e6cdcfcc38f67c6d709329e5",
+    "url": "/static/media/css.dd5dfed3.svg"
+  },
+  {
+    "revision": "832bcfaf005adc3d20a68f86fc3933a7",
+    "url": "/static/media/database.832bcfaf.svg"
+  },
+  {
+    "revision": "13030725a3439b51ae7943033399585d",
+    "url": "/static/media/github.13030725.svg"
+  },
+  {
+    "revision": "705b987e58fa317cf913c181018040be",
+    "url": "/static/media/icons8-nodejs.705b987e.svg"
+  },
+  {
+    "revision": "f2f74f8abf5e7f2cbe78b0640cd6e2a3",
+    "url": "/static/media/java.f2f74f8a.svg"
+  },
+  {
+    "revision": "4989cd37b1b6bac11fdf8342f7991873",
+    "url": "/static/media/javascript.4989cd37.svg"
+  },
+  {
+    "revision": "b10b9322ba3a1b0edf8a791896fa6e35",
+    "url": "/static/media/linkedin.b10b9322.svg"
+  },
+  {
+    "revision": "336888b98c144f86ec9bed01e1d311bb",
+    "url": "/static/media/rnative.336888b9.svg"
+  },
+  {
+    "revision": "7dea62dd3a5b782e56a2a5e62ae73a96",
+    "url": "/static/media/smartphone.7dea62dd.svg"
+  },
+  {
+    "revision": "49224b0e53aec159950c88ac30caaa03",
+    "url": "/static/media/twitter.49224b0e.svg"
+  }
+]);
